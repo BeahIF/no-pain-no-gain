4f4ff3e492142a557559123c8c8447cd916e8967
Igor Oliveira <igor_sousa@email.com>
2020-02-06 17:22:45 -0300

    Voltando para imagens locais a critério do cliente

--- app/src/app/image/image.page.ts
+++ app/src/app/image/image.page.ts
@@ -21,148 +21,128 @@ export class ImagePage implements OnInit {
     this.cards = [
       {
         "place": "Sem localização",
-        "image": "https://i.ibb.co/wgW8PYD/image-1.jpg",
+        "image": "assets/image/image_1.jpg",
         "description": "",
         "vr": "assets/vr/vr_1.jpg"
       },
       {
         "place": "32º01'31S - 52º06'21W",
-        "image": "https://i.ibb.co/6bJzD0G/image-2.jpg",
+        "image": "assets/image/image_2.jpeg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "Sem localização",
-        "image": "https://i.ibb.co/k27DKTm/image-3.jpg",
+        "image": "assets/image/image_3.jpeg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "Sem localização",
-        "image": "https://i.ibb.co/CB7krQP/image-4.jpg",
+        "image": "assets/image/image_4.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º00'02S - 52º08'14W",
-        "image": "https://i.ibb.co/0jBw2SQ/image-5.jpg",
+        "image": "assets/image/image_5.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "-32º04'09S - 52º09'35W",
-        "image": "https://i.ibb.co/TB2BZYG/image-6.jpg",
+        "image": "assets/image/image_6.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º10'54S - 52º04'47W",
-        "image": "https://i.ibb.co/58Q0Smf/image-7.jpg",
+        "image": "assets/image/image_7.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º08'58S- 52º06'03W",
-        "image": "https://i.ibb.co/2d7XknT/image-8.jpg",
+        "image": "assets/image/image_8.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º12'21S - 52º10'26W",
-        "image": "https://i.ibb.co/zGtsVtB/image-9.jpg",
+        "image": "assets/image/image_9.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º10'44S- 52º05'36W",
-        "image": "https://i.ibb.co/KztGd9Z/image-10.jpg",
+        "image": "assets/image/image_10.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º08'58S - 52º05'10W",
-        "image": "https://i.ibb.co/4KRxWs1/image-11.jpg",
+        "image": "assets/image/image_11.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º12'21S - 52º10'26W",
-        "image": "https://i.ibb.co/KVLzZZQ/image-12.jpg",
+        "image": "assets/image/image_12.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º10'14S - 52º07'42W",
-        "image": "https://i.ibb.co/k0FNfJS/image-13.jpg",
+        "image": "assets/image/image_13.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º01'01S - 52º08'17W",
-        "image": "https://i.ibb.co/RDfpKmf/image-14.jpg",
+        "image": "assets/image/image_14.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "Sem localização",
-        "image": "https://i.ibb.co/z224g2m/image-15.jpg",
+        "image": "assets/image/image_15.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "Sem localização",
-        "image": "https://i.ibb.co/kxF8dnQ/image-16.jpg",
+        "image": "assets/image/image_16.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "Sem localização",
-        "image": "https://i.ibb.co/5F8hzTQ/image-17.jpg",
+        "image": "assets/image/image_17.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º09'42S - 52º06'00W",
-        "image": "https://i.ibb.co/3c74M55/image-18.jpg",
+        "image": "assets/image/image_18.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º09'57S - 52º05'51W",
-        "image": "https://i.ibb.co/MfcTwmJ/image-19.jpg",
+        "image": "assets/image/image_19.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º08'40S - 52º04'11W",
-        "image": "https://i.ibb.co/fGg2rBg/image-20.jpg",
+        "image": "assets/image/image_20.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       },
       {
         "place": "32º12'21S - 52º10'26W",
-        "image": "https://i.ibb.co/zGtsVtB/image-21.jpg",
+        "image": "assets/image/image_21.jpg",
         "description": "",
-        "button": "false",
         "vr": ""
       }
     ]     
