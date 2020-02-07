e1ec042b4128163ab7ff5b6cfd8448d06b13441e
Felipe Vencato <felipevencato@gmail.com>
2020-02-07 08:20:17 -0300

    Janela de informações com as imagens

--- app/src/app/map/map.page.ts
+++ app/src/app/map/map.page.ts
@@ -29,10 +29,10 @@ export class MapPage implements OnInit {
   loadMap() {
     this.map = GoogleMaps.create('map_canvas', {
       camera: {
-        target: {lat: -31.100825, lng: -51.315300},
-        zoom: 7
+        target: {lat: -32.065233, lng: -52.111842},
+        zoom: 9
       },
-      mapType: 'HYBRID',
+      mapType: 'SATELLITE',
       'gestures': {
         'scroll': true,
         'tilt': true,
@@ -40,94 +40,49 @@ export class MapPage implements OnInit {
         'zoom': true
       }
     });
-    this.map.setMapTypeId ('HYBRID');
-    let htmlInfoWindow = new HtmlInfoWindow();
-
-    // flip-flop contents
-    // https://davidwalsh.name/css-flip
-    let frame: HTMLElement = document.createElement('div');
-    frame.innerHTML = `
-<div class="flip-container" id="flip-container">
-  <div class="flipper">
-    <div class="front">
-    <h3>Titulo!</h3>
-    <img src="https://ionicframework.com/docs/demos/api/card/madison.jpg">
-  </div>
-  <div class="back">
-    <!-- back content -->
-    Marcador legal ;)
-    </div>
-  </div>
-</div>`;
-    frame.addEventListener("click", (evt) => {
-      let container = document.getElementById('flip-container');
-      if (container.className.indexOf(' hover') > -1) {
-        container.className = container.className.replace(" hover", "");
-      } else {
-        container.className += " hover";
-      }
-    });
-    htmlInfoWindow.setContent(frame, {
-      width: "170px"
-    });
-    /*
-    1-Sem localização-https://i.ibb.co/wgW8PYD/image-1.jpg
-    2- -32.025278 -52.105833-https://i.ibb.co/6bJzD0G/image-2.jpg
-    3-Sem localização-https://i.ibb.co/k27DKTm/image-3.jpg
-    4-Sem localização-https://i.ibb.co/CB7krQP/image-4.jpg
-    5- -32.000556  -52.137222-https://i.ibb.co/0jBw2SQ/image-5.jpg
-    6- -32.069167 -52.159722-https://i.ibb.co/TB2BZYG/image-6.jpg
-    7--32.181667 -52.079722-https://i.ibb.co/58Q0Smf/image-7.jpg
-    8- -32.149444 -52.100833-https://i.ibb.co/2d7XknT/image-8.jpg
-    9- -32.205833 -52.173889-https://i.ibb.co/zGtsVtB/image-9.jpg
-    10- -32.178889 -52.093333-https://i.ibb.co/KztGd9Z/image-10.jpg
-    11- -32.149444 -52.086111-https://i.ibb.co/4KRxWs1/image-11.jpg
-    12- -32.205833 -52.173889-https://i.ibb.co/KVLzZZQ/image-12.jpg
-    13- -32.170556 -52.128333-https://i.ibb.co/k0FNfJS/image-13.jpg
-    14- -32.016944 -52.138056-https://i.ibb.co/RDfpKmf/image-14.jpg
-    15-Sem localização-https://i.ibb.co/z224g2m/image-15.jpg
-    16-Sem localização-https://i.ibb.co/kxF8dnQ/image-16.jpg
-    17-Sem localização-https://i.ibb.co/5F8hzTQ/image-17.jpg
-    18- -32.161667 -52.100000-https://i.ibb.co/3c74M55/image-18.jpg
-    19- -32.165833  -52.097500-https://i.ibb.co/MfcTwmJ/image-19.jpg
-    20- -32.144444  -52.069722-https://i.ibb.co/fGg2rBg/image-20.jpg
-    21- -32.205833 -52.173889-https://i.ibb.co/zGtsVtB/image-21.jpg
-    */
-    let markerData:Array<Object>;
-    markerData = [
-    { position: {lat: -32.025278, lng: -52.105833}, url: "https://i.ibb.co/6bJzD0G/image-2.jpg"},
-    { position: {lat: -32.000556, lng: -52.137222}, url: "https://i.ibb.co/0jBw2SQ/image-5.jpg"},
-    { position: {lat: -32.069167, lng: -52.159722}, url: "https://i.ibb.co/TB2BZYG/image-6.jpg"},
-    { position: {lat: -32.181667, lng: -52.079722}, url: "https://i.ibb.co/58Q0Smf/image-7.jpg"},
-    { position: {lat: -32.149444, lng: -52.100833}, url: "https://i.ibb.co/2d7XknT/image-8.jpg"},
-    { position: {lat: -32.205833, lng: -52.173889}, url: "https://i.ibb.co/zGtsVtB/image-9.jpg"},
-    { position: {lat: -32.178889, lng: -52.093333}, url: "https://i.ibb.co/KztGd9Z/image-10.jpg"},
-    { position: {lat: -32.149444, lng: -52.086111}, url: "https://i.ibb.co/4KRxWs1/image-11.jpg"},
-    { position: {lat: -32.205833, lng: -52.173889}, url: "https://i.ibb.co/KVLzZZQ/image-12.jpg"},
-    { position: {lat: -32.170556, lng: -52.128333}, url: "https://i.ibb.co/k0FNfJS/image-13.jpg"},
-    { position: {lat: -32.016944, lng: -52.138056}, url: "https://i.ibb.co/RDfpKmf/image-14.jpg"},
-    { position: {lat: -32.161667, lng: -52.100000}, url: "https://i.ibb.co/3c74M55/image-18.jpg"},
-    { position: {lat: -32.165833, lng: -52.097500}, url: "https://i.ibb.co/MfcTwmJ/image-19.jpg"},
-    { position: {lat: -32.144444, lng: -52.069722}, url: "https://i.ibb.co/fGg2rBg/image-20.jpg"},
-    { position: {lat: -32.205833, lng: -52.173889}, url: "https://i.ibb.co/zGtsVtB/image-21.jpg"}
+    let markerData = [
+      [-32.025278, -52.105833, "assets/image/image_2.jpg"],
+      [-32.000556, -52.137222, "assets/image/image_5.jpg"],
+      [-32.069167, -52.159722, "assets/image/image_6.jpg"],
+      [-32.181667, -52.079722, "assets/image/image_7.jpg"],
+      [-32.149444, -52.100833, "assets/image/image_8.jpg"],
+      [-32.205833, -52.173889, "assets/image/image_9.jpg"],
+      [-32.178889, -52.093333, "assets/image/image_10.jpg"],
+      [-32.149444, -52.086111, "assets/image/image_11.jpg"],
+      [-32.205833, -52.173889, "assets/image/image_12.jpg"],
+      [-32.170556, -52.128333, "assets/image/image_13.jpg"],
+      [-32.016944, -52.138056, "assets/image/image_14.jpg"],
+      [-32.161667, -52.100000, "assets/image/image_18.jpg"],
+      [-32.165833, -52.097500, "assets/image/image_19.jpg"],
+      [-32.144444, -52.069722, "assets/image/image_20.jpg"],
+      [-32.205833, -52.173889, "assets/image/image_21.jpg"]
     ];
-
     let markers = new Array();
-    markerData.forEach(item => 
-      markers.push ({
-        position: item["position"],
+    markerData.forEach(item => markers.push ({
+        position: {lat: item[0], lng: item[1]},
         draggable: false,
-        disableAutoPan: false
+        disableAutoPan:true,
+        img: item[2]
       })
     );
-    console.log(markers);
+    let htmlInfoWindow = new HtmlInfoWindow();
+    
     markers.forEach (item => 
-      this.map.addMarker(item).then((marker: Marker) => {
+      this.map.addMarker(item)
+      .then((marker: Marker) => {
         marker.on(GoogleMapsEvent.MARKER_CLICK)
         .subscribe(() => {
-            htmlInfoWindow.open(marker);
+          let img = item["img"];
+          let html = [
+            '<div style="width:200px;">',
+            '<img src="' + img + '"/>',
+            '</div>'
+          ].join("");
+          htmlInfoWindow.setContent(html);
+          htmlInfoWindow.open(marker);
         });
-        marker.trigger(GoogleMapsEvent.MARKER_CLICK);
+        //Para iniciar o mapa com a janela de informação aberta descomente a linha abaixo
+        //marker.trigger(GoogleMapsEvent.MARKER_CLICK);
       })
     );
   }
